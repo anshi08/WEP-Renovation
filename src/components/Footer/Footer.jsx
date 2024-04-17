ee64df4105f1a2da51c1d142699341d0b30f909e
anshi08 <sabharwalanshika08@gmail.com>
2024-04-16 19:00:19 -0700

    Footer Img added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import { Link, NavLink } from "react-router-dom";
 import './Footer.css'
 import EastIcon from '@mui/icons-material/East';
+import TwitterImg from '../../assets/image (7).svg'
+import LinkedInImg from '../../assets/image (8).svg'
 
 
 export default function Footer() {
@@ -44,17 +46,11 @@ export default function Footer() {
                     {/* Fourth sub-div */}
                     <div className="flex flex-col">
                         <div className="flex flex-row">
-                            <Link to="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
-                                    <path fillRule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clipRule="evenodd" />
-                                </svg>
-                                <span className="sr-only">Twitter page</span>
+                            <Link to="https://twitter.com/quadrealgroup" >
+                               <img src={LinkedInImg} />
                             </Link>
-                            <Link to="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M5.93 17.08H2.5V7.56h3.43v9.52zM4.22 6.21a1.65 1.65 0 0 1-1.65-1.65 1.65 1.65 0 0 1 1.65-1.65c1.15 0 1.65.84 1.65 1.65s-.5 1.65-1.65 1.65zm15.08 10.87h-3.42v-5.97c0-1.42-.5-2.39-1.76-2.39-1.1 0-1.76.74-2.05 1.46-.1.24-.13.58-.13.92v6.98h-3.42s.05-10.52 0-11.57h3.42v1.64c.45-.7 1.26-1.7 3.08-1.7 2.24 0 3.92 1.47 3.92 4.63v7h-.01z" />
-                                </svg>
-                                <span className="sr-only">LinkedIn page</span>
+                            <Link to="https://www.linkedin.com/company/quadreal/" >
+                               <img src={TwitterImg} style={{marginLeft:'1rem'}}/>
                             </Link>
                         </div>
                         <Link to="https://www.quadreal.com/legal/"
