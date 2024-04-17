48a6a0283b16967467680ef960f478573e68ca6d
anshi08 <sabharwalanshika08@gmail.com>
2024-04-16 17:06:26 -0700

    Home Page Done

--- src/components/Home/NoDropOutzone.jsx
+++ src/components/Home/NoDropOutzone.jsx
@@ -1,28 +1,65 @@
 import React from 'react'
 import Image1 from "../../assets/Image23.png"
-import { Typography } from '@mui/material'
+import { motion } from 'framer-motion'
 import "./NoDrop.css"
+import { Grid } from '@mui/material'
 
 const NoDropOutzone = () => {
     return (
         <>
-            <div className='flex flex-row justify-evenly flex-wrap' >
-                <div className='flex flex-col' >
-                    <img src={Image1} style={{ width: '637px' }} />
-                    <Typography className='footertext'>NO DROP-OUT ZONES</Typography>
-                </div>
-                <div className='flex flex-col' >
-                    <div className='line my-6'></div>
-                    <p className='SmartTechText'>
-                        With SMART technology incorporated throughout the WEP complex, people will be able to enjoy hands-free
-                        “wave and open” access points, contactless food ordering and payment for speedy delivery. With high
-                        speed WI FI available throughout the public realm, and no drop-out-zones, tenants and guests will enjoy
-                        all the freedom today’s technology brings. Our SMART approach extends to creating a more sustainable,
-                        energy efficient, and healthier building through an optimized energy management plan based on a variety
-                        of sensors and data collection - a win win for the environment and all occupants.
-                    </p>
-                </div>
-            </div>
+            <Grid container>
+                <Grid item xs={12} md={6}>
+
+                    <motion.div
+                        initial={{
+                            opacity: 0,
+                            x: -100,
+                        }}
+                        whileInView={{
+                            opacity: 1,
+                            x: 0,
+                            transition: {
+                                duration: 1,
+                            },
+                        }}
+                        viewport={{ once: true }}
+                        className='flex flex-col items-center'
+                    >
+                        <img src={Image1} style={{ width: '80%' }} />
+                    </motion.div>
+
+                    <p className='footertext'>NO DROP-OUT ZONES</p>
+                </Grid>
+                <Grid item xs={12} md={6} display='flex' justifyContent='center' alignItems='center' position='relative'>
+                    <div className='lineNoDrop my-6'></div>
+
+                    <motion.div
+
+                        initial={{
+                            opacity: 0,
+                            x: 100,
+                        }}
+                        whileInView={{
+                            opacity: 1,
+                            x: 0,
+                            transition: {
+                                duration: 1,
+                            },
+                        }}
+
+                        viewport={{ once: true }}
+                    >
+                        <p className='SmartTechText my-8'>
+                            With SMART technology incorporated throughout the WEP complex, people will be able to enjoy hands-free
+                            “wave and open” access points, contactless food ordering and payment for speedy delivery. With high
+                            speed WI FI available throughout the public realm, and no drop-out-zones, tenants and guests will enjoy
+                            all the freedom today’s technology brings. Our SMART approach extends to creating a more sustainable,
+                            energy efficient, and healthier building through an optimized energy management plan based on a variety
+                            of sensors and data collection - a win win for the environment and all occupants.
+                        </p>
+                    </motion.div>
+                </Grid>
+            </Grid>
         </>
     )
 }
